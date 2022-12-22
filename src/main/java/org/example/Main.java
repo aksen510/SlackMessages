7941426d68176fedd1fb50b9e23766c1a16e562e
aksen510 <510cara@naver.com>
2022-12-22 17:30:43 +0900

    [FEAT] Add blockAsString

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import com.slack.api.Slack;
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.jetty.SlackAppServer;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import com.slack.api.model.Message;
@@ -30,10 +32,22 @@ import static com.slack.api.model.block.element.BlockElements.*;
 public class Main {
 
     private static final String Token = "xoxb-1376359396183-4555396726737-OGvVgYlP2HI5alnqM1dLvJ5f";
+    private static final String Signing = "1653988ebd51184492c16e6108d431cf";
     private static Slack slack = Slack.getInstance();
     //private static List<LayoutBlock> layoutBlocks = new ArrayList<>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+
+        // App expects env variables (SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET)
+//        App app = new App();
+//
+//        app.command("/hello", (req, ctx) -> {
+//            return ctx.ack(":wave: Hello!");
+//        });
+//
+//        SlackAppServer server = new SlackAppServer(app);
+//        server.start(); // http://localhost:3000/slack/events
+
 
         /*
         layoutBlocks.add(section(section -> section.text(markdownText("새로운 의견이 등록되었습니다."))));
@@ -81,32 +95,36 @@ public class Main {
                                             "\t\t{\n" +
                                             "\t\t\t\"type\": \"section\",\n" +
                                             "\t\t\t\"text\": {\n" +
-                                            "\t\t\t\t\"type\": \"plain_text\",\n" +
-                                            "\t\t\t\t\"text\": \"배송팁 등록이 승인되었습니다.\",\n" +
-                                            "\t\t\t\t\"emoji\": true\n" +
+                                            "\t\t\t\t\"type\": \"mrkdwn\",\n" +
+                                            "\t\t\t\t\"text\": \"안녕하세요! \\n 슬랙봇입니다:)\\n\"\n" +
+                                            "\t\t\t},\n" +
+                                            "\t\t\t\"accessory\": {\n" +
+                                            "\t\t\t\t\"type\": \"image\",\n" +
+                                            "\t\t\t\t\"image_url\": \"https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg\",\n" +
+                                            "\t\t\t\t\"alt_text\": \"cute cat\"\n" +
                                             "\t\t\t}\n" +
                                             "\t\t},\n" +
                                             "\t\t{\n" +
-                                            "\t\t\t\"type\": \"divider\"\n" +
+                                            "\t\t\t\"type\": \"context\",\n" +
+                                            "\t\t\t\"elements\": [\n" +
+                                            "\t\t\t\t{\n" +
+                                            "\t\t\t\t\t\"type\": \"plain_text\",\n" +
+                                            "\t\t\t\t\t\"text\": \"↓↓↓\",\n" +
+                                            "\t\t\t\t\t\"emoji\": true\n" +
+                                            "\t\t\t\t}\n" +
+                                            "\t\t\t]\n" +
                                             "\t\t},\n" +
                                             "\t\t{\n" +
                                             "\t\t\t\"type\": \"section\",\n" +
                                             "\t\t\t\"text\": {\n" +
-                                            "\t\t\t\t\"type\": \"plain_text\",\n" +
-                                            "\t\t\t\t\"text\": \"버튼을 클릭해주세요!\",\n" +
-                                            "\t\t\t\t\"emoji\": true\n" +
-                                            "\t\t\t},\n" +
-                                            "\t\t\t\"accessory\": {\n" +
-                                            "\t\t\t\t\"type\": \"button\",\n" +
-                                            "\t\t\t\t\"text\": {\n" +
-                                            "\t\t\t\t\t\"type\": \"plain_text\",\n" +
-                                            "\t\t\t\t\t\"text\": \"승인\",\n" +
-                                            "\t\t\t\t\t\"emoji\": true\n" +
-                                            "\t\t\t\t},\n" +
-                                            "\t\t\t\t\"value\": \"action_approve\",\n" +
-                                            "\t\t\t\t\"style\": \"primary\",\n" +
-                                            "\t\t\t\t\"action_id\": \"actionId-2\"\n" +
+                                            "\t\t\t\t\"type\": \"mrkdwn\",\n" +
+                                            "\t\t\t\t\"text\": \":thumbsup: *굵은 글씨*, and ~삭제~, and <https://naver.com|네이버 페이지로 이동>\"\n" +
                                             "\t\t\t}\n" +
+                                            "\t\t},\n" +
+                                            "\t\t{\n" +
+                                            "\t\t\t\"type\": \"image\",\n" +
+                                            "\t\t\t\"image_url\": \"https://i1.wp.com/thetempest.co/wp-content/uploads/2017/08/The-wise-words-of-Michael-Scott-Imgur-2.jpg?w=1024&ssl=1\",\n" +
+                                            "\t\t\t\"alt_text\": \"돋보기\"\n" +
                                             "\t\t}\n" +
                                             "\t]")
                     );
@@ -114,6 +132,15 @@ public class Main {
             if (response.isOk()){
                 Message postedMessage = response.getMessage();
                 System.out.println("전송 성공");
+
+//                app.blockAction("actionId-2", (req, ctx) -> {
+//                    String value = req.getPayload().getActions().get(0).getValue(); // "button's value"
+//                    if (req.getPayload().getResponseUrl() != null) {
+//                        // Post a message to the same channel if it's a block in a message
+//                        ctx.respond("You've sent " + value + " by clicking the button!");
+//                    }
+//                    return ctx.ack();
+//                });
             }
             else {
                 String errorCode = response.getError();
